8955f1590e348437330558872c62c54d446b510e
sanghunkang <sanghunkang.dev@gmail.com>
2019-12-17 23:51:02 +0900

    removed graphql components

--- src/content/RepoPage/RepoPage.js
+++ src/content/RepoPage/RepoPage.js
@@ -1,42 +1,42 @@
 import React, { useState } from 'react';
 import RepoTable from './RepoTable';
-import { gql } from 'apollo-boost';
-import { Query } from 'react-apollo';
+// import { gql } from 'apollo-boost';
+// import { Query } from 'react-apollo';
 import { Link, DataTableSkeleton, Pagination } from 'carbon-components-react';
 
-const REPO_QUERY = gql`
-  query REPO_QUERY {
-    # Let's use carbon as our organization
-    organization(login: "carbon-design-system") {
-      # We'll grab all the repositories in one go. To load more resources
-      # continuously, see the advanced topics.
-      repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
-        totalCount
-        nodes {
-          url
-          homepageUrl
-          issues(filterBy: { states: OPEN }) {
-            totalCount
-          }
-          stargazers {
-            totalCount
-          }
-          releases(first: 1) {
-            totalCount
-            nodes {
-              name
-            }
-          }
-          name
-          updatedAt
-          createdAt
-          description
-          id
-        }
-      }
-    }
-  }
-`;
+// const REPO_QUERY = gql`
+//   query REPO_QUERY {
+//     # Let's use carbon as our organization
+//     organization(login: "carbon-design-system") {
+//       # We'll grab all the repositories in one go. To load more resources
+//       # continuously, see the advanced topics.
+//       repositories(first: 75, orderBy: { field: UPDATED_AT, direction: DESC }) {
+//         totalCount
+//         nodes {
+//           url
+//           homepageUrl
+//           issues(filterBy: { states: OPEN }) {
+//             totalCount
+//           }
+//           stargazers {
+//             totalCount
+//           }
+//           releases(first: 1) {
+//             totalCount
+//             nodes {
+//               name
+//             }
+//           }
+//           name
+//           updatedAt
+//           createdAt
+//           description
+//           id
+//         }
+//       }
+//     }
+//   }
+// `;
 
 const headers = [
   {
@@ -90,6 +90,36 @@ const getRowItems = rows =>
     links: <LinkList url={row.url} homepageUrl={row.homepageUrl} />,
   }));
 
+  const rows = [
+    {
+      id: '1',
+      name: 'Repo 1',
+      createdAt: 'Date',
+      updatedAt: 'Date',
+      issueCount: '123',
+      stars: '456',
+      links: 'Links',
+    },
+    {
+      id: '2',
+      name: 'Repo 2',
+      createdAt: 'Date',
+      updatedAt: 'Date',
+      issueCount: '123',
+      stars: '456',
+      links: 'Links',
+    },
+    {
+      id: '3',
+      name: 'Repo 3',
+      createdAt: 'Date',
+      updatedAt: 'Date',
+      issueCount: '123',
+      stars: '456',
+      links: 'Links',
+    },
+  ];
+
 const RepoPage = () => {
   const [totalItems, setTotalItems] = useState(0);
   const [firstRowIndex, setFirstRowIndex] = useState(0);
@@ -99,7 +129,7 @@ const RepoPage = () => {
     <div className="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
       <div className="bx--row repo-page__r1">
         <div className="bx--col-lg-16">
-          <Query query={REPO_QUERY}>
+          {/* <Query query={REPO_QUERY}>
             {({ loading, error, data: { organization } }) => {
               // Wait for the request to complete
               if (loading)
@@ -120,7 +150,7 @@ const RepoPage = () => {
               const rows = getRowItems(repositories.nodes? repositories.nodes: []);
 
               return (
-                <>
+                <> */}
                   <RepoTable
                     headers={headers}
                     rows={rows.slice(
@@ -142,10 +172,10 @@ const RepoPage = () => {
                       setFirstRowIndex(pageSize * (page - 1));
                     }}
                   />
-                </>
+                {/* </>
               );
             }}
-          </Query>
+          </Query> */}
         </div>
       </div>
     </div>
